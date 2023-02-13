0d3768d96502fed969d35e0f424f17dddd5b78e3
Gabriel <gabrielgarciaofc@gmail.com>
2023-02-13 12:10:25 -0300

    consulta simples com Query

--- src/main/java/com/gabrielgarcia/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gabrielgarcia/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.gabrielgarcia.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gabrielgarcia.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.gabrielgarcia.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-	public List<Post> findByTituloContainingIgnoreCase(String texto);
+	@Query("{ 'titulo': { $regex: ?0, $options: 'i' } }") // ?0 é o primeiro parâmetro que vem no método o <i> para maiúsculas e minúsculas, para mais informacoes consultar a documentacao do mongodb 
+	public List<Post> findByTitulo(String texto);
+	
+	public List<Post> findByTituloContainingIgnoreCase(String texto); // Gerado pelos query métodos 
 }
